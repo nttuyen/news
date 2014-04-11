6624dcb798c853cfd9fe09c69d2483a3fb551251
nttuyen <nttuyen_it@yahoo.com>
2014-04-12 00:43:17 +0700

    add test login with api #9

---
+++ rss-reader/src/main/java/com/nttuyen/news/Consts.java
@@ -0,0 +1,9 @@
+package com.nttuyen.news;
+
+/**
+ * @author nttuyen266@gmail.com
+ */
+public interface Consts {
+    String REST_BASE_URL = "http://localhost:86/administrator";
+    String REST_ROOT_URL = REST_BASE_URL + "/index.php";
+}
---
+++ rss-reader/src/test/java/com/nttuyen/news/rest/TestRestLoginApi.java
@@ -0,0 +1,43 @@
+package com.nttuyen.news.rest;
+
+import com.nttuyen.news.Consts;
+import com.nttuyen.news.http.HttpException;
+import com.nttuyen.news.http.HttpExecutor;
+import com.nttuyen.news.http.HttpExecutorFactory;
+import com.nttuyen.news.http.HttpRequest;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author nttuyen266@gmail.com
+ */
+public class TestRestLoginApi {
+    private static final Logger log = Logger.getLogger(TestRestLoginApi.class);
+    private HttpExecutor executor;
+
+    @Before
+    public void before() {
+        executor = HttpExecutorFactory.createDefault();
+    }
+
+    @Test
+    public void testLogin() {
+        final String url = Consts.REST_ROOT_URL;
+        HttpRequest request = new HttpRequest(url, HttpRequest.Method.POST);
+        request.addParam("option", "com_login");
+        request.addParam("task", "api.login");
+        //request.addParam("format", "json");
+        request.addParam("username", "crawler");
+        request.addParam("password", "123456");
+        try {
+            executor.execute(request, (response) -> {
+                Assert.assertEquals(200, response.getStatusCode());
+            });
+        } catch (HttpException e) {
+            log.error(e);
+            Assert.fail("Exception when try login");
+        }
+    }
+}
