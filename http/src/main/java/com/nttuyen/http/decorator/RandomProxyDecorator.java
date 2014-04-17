fe90dea0dbccba7611ea55ee6983d64fcac36de9
nttuyen <tuyennt@exoplatform.com>
2014-04-17 14:00:20 +0700

    Implement Proxy decorator #21

--- http/src/main/java/com/nttuyen/http/decorator/RandomProxyDecorator.java
+++ http/src/main/java/com/nttuyen/http/decorator/RandomProxyDecorator.java
@@ -1,23 +1,7 @@
-/*
- * Copyright (C) 2012 eXo Platform SAS.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
 package com.nttuyen.http.decorator;
 
+import java.util.ArrayList;
+import java.util.List;
 import com.nttuyen.http.Executor;
 import com.nttuyen.http.ExecutorDecorator;
 import com.nttuyen.http.HttpException;
@@ -30,30 +14,66 @@ import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.conn.routing.HttpRoutePlanner;
 
 public class RandomProxyDecorator extends ExecutorDecorator {
-  @Override
-  public ExecutorDecorator setExecutor(Executor executor) {
-    super.setExecutor(executor);
-    if(this.getExecutor() instanceof HttpClientExecutor) {
-      HttpClientExecutor exe = (HttpClientExecutor)this.getExecutor();
-      exe.getBuilder().setRoutePlanner(routePlanner);
-    }
-      return this;
-  }
+    private final List<Proxy> proxies = new ArrayList<>();
 
-  @Override
-  public Response execute(Request request) throws HttpException {
-    //Force create new HttpClient
-    if(this.getExecutor() instanceof HttpClientExecutor) {
-      HttpClientExecutor exe = (HttpClientExecutor)this.getExecutor();
-      HttpClient client = exe.getBuilder().build();
-      exe.setHttpClient(client);
+    public RandomProxyDecorator addProxy(String host, int port) {
+        proxies.add(new Proxy(host, port));
+        return this;
     }
-    return this.executor.execute(request);
-  }
 
-  private HttpRoutePlanner routePlanner = (target, request, context) -> {
-        //TODO: how to pick random proxy
-        return new HttpRoute(target, null,  new HttpHost("someproxy", 8080),
-              "https".equalsIgnoreCase(target.getSchemeName()));
+    @Override
+    public ExecutorDecorator setExecutor(Executor executor) {
+        super.setExecutor(executor);
+        if (this.getExecutor() instanceof HttpClientExecutor) {
+            HttpClientExecutor exe = (HttpClientExecutor) this.getExecutor();
+            exe.getBuilder().setRoutePlanner(routePlanner);
+        }
+        return this;
+    }
+
+    @Override
+    public Response execute(Request request) throws HttpException {
+        //Force create new HttpClient
+        if (this.getExecutor() instanceof HttpClientExecutor) {
+            HttpClientExecutor exe = (HttpClientExecutor) this.getExecutor();
+            HttpClient client = exe.getBuilder().build();
+            exe.setHttpClient(client);
+        }
+        return this.executor.execute(request);
+    }
+
+    private HttpRoutePlanner routePlanner = (target, request, context) -> {
+        if(proxies.size() > 0) {
+            int index = (int)(System.currentTimeMillis() % proxies.size());
+            Proxy proxy = proxies.get(index);
+            return new HttpRoute(target, null, new HttpHost(proxy.host, proxy.port), "https".equalsIgnoreCase(target.getSchemeName()));
+        } else {
+            return new HttpRoute(target);
+        }
     };
+
+    private static class Proxy {
+        final String host;
+        final int port;
+
+        Proxy(String host, int port) {
+            this.host = host;
+            this.port = port;
+        }
+
+        @Override
+        public int hashCode() {
+            return new StringBuilder(host).append(":").append(port).toString().hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj == this) return true;
+            if(obj instanceof Proxy) {
+                Proxy p = (Proxy)obj;
+                return ((host == null && p.host == null) || host.equals(p.host)) && port == p.port;
+            }
+            return false;
+        }
+    }
 }
